0e19bac9bfb6dfae777bbd1327c3848e7422870a
karthikofr <40591135+karthikofr@users.noreply.github.com>
2018-10-15 14:44:07 +0530

    Add files via upload

---
+++ Contact.aspx.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Mail;
+
+namespace OfrDisign
+{
+    public partial class Contact : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            MailMessage msg = new MailMessage();
+
+
+            //sender email address
+            msg.From = new MailAddress(TextBox2.Text);
+            //Recipient email address
+            msg.To.Add("contact@ofrapp.com");
+            msg.Subject = TextBox3.Text;
+            msg.Body = "Name: " + TextBox1.Text + "\nFrom: " + TextBox2.Text + "\nMessage: " + TextBox4.Text;
+
+
+
+
+            //your remote server smtp server ip
+            SmtpClient smtp = new SmtpClient();
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.Credentials = new System.Net.NetworkCredential("michaelgofr@gmail.com", "7795249636");
+            smtp.EnableSsl = true;
+            smtp.Send(msg);
+            //clear fields
+
+
+
+            ScriptManager.RegisterStartupScript(this, this.GetType(), "popup", "alert('message sent succefully Thank you for contact us');", true);
+            TextBox1.Text = TextBox2.Text = TextBox3.Text = TextBox4.Text = "";
+        }
+    }
+}
